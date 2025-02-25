f479ace2c33b02d4fd0a41aac63a322cf3b0d6ce
sebastianpinzon19 <sebastian_pinzonr@.sena.edu.co>
2025-02-25 11:35:54 -0500

    feat(main.kt):for each

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -8,15 +8,27 @@ import question.question.estudiantenv3.edad
 
 
 fun main() {
-    //ejecutarNivel1()
-    //ejecutarNivel2()
-    //ejecutarNivel3()
-    val estudiantenv3 = estudiantenv3.Estudiante()
-    println(estudiantenv3.nombre)
-    println(edad.)
+//    //ejecutarNivel1()
+//    //ejecutarNivel2()
+//    //ejecutarNivel3()
+//    val estudiantenv3 = estudiantenv3.Estudiante()
+//    println(estudiantenv3.nombre)
+//    println(edad.)
+//
+//
+    val nombres = listOf("Ana", "Juan", "Pedro", "María")
 
+    // Usando forEach
+    nombres.forEach { nombre ->
+        println(nombre)
+    }
 
-
+    // También puedes usar el it implícito
+    nombres.forEach {
+        println(it)
+    }
 }
+
+
 
 
